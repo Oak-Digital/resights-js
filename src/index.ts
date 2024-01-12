caa467c8a62561448ee81583d04f21f89312c9f0
Alexander Nortung <alexander.nortung@oakdigital.dk>
2024-01-12 11:43:03 +0100

    fix: Updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { Resights } from './client'
-export * from './generated/core/ApiError'
-export * from './generated/core/ApiRequestOptions'
-export * from './generated/core/ApiResult'
+export * from './generated'
